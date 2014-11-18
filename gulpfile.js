a668fe81179c13ce7026d456f00f0006e943631e
hwclass <harderworking.class@gmail.com>
2014-11-18 08:24:40 +0200

    Added the bowerrc and gulpfile files.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat'),
+    rimraf = require('gulp-rimraf'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+var express = require('express'),
+    refresh = require('gulp-livereload'),
+    livereload = require('connect-livereload'),
+    livereloadport = 35729,
+    serverport = 5000;

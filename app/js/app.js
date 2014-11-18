beae12beea40fca40780794812c441aa9039f435
hwclass <harderworking.class@gmail.com>
2014-11-18 14:59:47 +0200

    Added tasks into gulp file.

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var angular = require('angular');
  
 var TestCtrl = require('./pages/test/controllers/TestCtrl');
--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,22 @@ var express = require('express'),
     livereload = require('connect-livereload'),
     livereloadport = 35729,
     serverport = 5000;
+
+// Dev task
+gulp.task('dev', ['browserify'], function() { });
+
+// Browserify task
+gulp.task('browserify', function() {
+  // Single point of entry (make sure not to src ALL your files, browserify will figure it out)
+  gulp.src(['app/js/app.js'])
+  .pipe(browserify({
+    insertGlobals: true,
+    debug: false
+  }))
+  // Bundle to a single file
+  .pipe(concat('bundle.js'))
+  // Output it to our dist folder
+  .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('default', ['dev']);

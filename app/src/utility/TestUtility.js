721b6a238c569ba3d8f9045eb361333ef4403d7a
hwclass <harderworking.class@gmail.com>
2014-12-10 01:37:04 +0200

    Modified the TestUtility utility.

--- app/src/utility/TestUtility.js
+++ app/src/utility/TestUtility.js
@@ -1,10 +1,28 @@
+/**
+ * @name TestUtility
+ * @desc TestUtility utility
+ */
+
 'use strict';
 
-module.exports = {
-	isNull : function (obj) {
-		return (typeof obj === 'null' ? true : false);
-	},
-	isUndefined : function (obj) {
-		return (typeof obj === 'undefined' ? true : false);
-	}
+var TestUtility = function () {
+
+  return(function () {
+
+    return {
+
+      isNull : function (obj) {
+        return (typeof obj === 'null' ? true : false);
+      },
+      
+      isUndefined : function (obj) {
+        return (typeof obj === 'undefined' ? true : false);
+      }
+
+    }
+
+  })();
+
 };
+
+module.exports = TestUtility;

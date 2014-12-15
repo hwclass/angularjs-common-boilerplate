cd3d3a97a970d75c4bb9297cb42ee73f00c80d63
hwclass <harderworking.class@gmail.com>
2014-12-15 16:32:57 +0200

    Added getUserAddress method into the AddressServiceProvider and AddressService

--- app/src/provider/AddressServiceProvider.js
+++ app/src/provider/AddressServiceProvider.js
@@ -29,6 +29,22 @@ var AddressServiceProvider = function ($http) {
         AddressService($http).testRequest(function (data) {
           callback(data);
         });
+      },
+
+      /**
+       * @name getUserAddress
+       * @ref getUserAddress
+       * @desc This method gets the address of the coming user
+       * @param {String} userName - User name value
+       * @param {String} passWord - Password value
+       * @param {String} prefix - Prefix value
+       * @param {Function} callback - Callback after response returns
+       * @returns {undefined}
+       */
+      getUserAddress : function (callback) {
+      	AddressService($http).getUserAddress(function (data) {
+      	  callback(data);
+      	});
       }
 
     }
--- app/src/service/AddressService.js
+++ app/src/service/AddressService.js
@@ -22,6 +22,28 @@ var AddressService = function ($http) {
         error(function (data, status, headers, config) {
           console.log(data);
         });
+      },
+
+      /*
+      * Phase 1.V3 - 3.1 Get User Address
+      */
+      getUserAddress : function (userSession, callback) {
+    	  $http({
+          method : 'GET', 
+          url : 'url',
+          data : {
+            'session_key' : userSession.getSessionKey()
+          }
+        }).
+        success(function(data, status, headers, config) {
+          /*
+          * Output : address_id, first_name, last_name, address, city, township, postal_code, phone_area_code, phone_number, response_code, errors
+          */
+          callback(data);
+        }).
+        error(function(data, status, headers, config) {
+          console.log(data);
+        });
       }
 
     }

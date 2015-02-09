4480e109fdffaa0fa42a4f2469d02051634b18fe
hwclass <harderworking.class@gmail.com>
2015-02-09 12:01:12 +0200

    Added comment into the TestComponent.

--- app/src/component/TestComponent.js
+++ app/src/component/TestComponent.js
@@ -5,6 +5,12 @@
 
 'use strict';
 
+/**
+* @name TestComponent
+* @ref TestComponent
+* @desc Does something awesome
+* @returns template
+*/
 var TestComponent = function () {
   return {
     template: 'Name: {{name}} Surname : {{surName}} Address: {{address}}'
